7318ca4d0021bfbe963d149790477d0cee96a097
Simon Blasby <simon.blasby@gmail.com>
2023-11-27 11:14:42 -0500

    finished the 'scrollspy' added js file and formatted the 'scrollspy' nav bar

---
+++ DojoWebsite/wwwroot/js/About.js
@@ -0,0 +1,29 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+    const sectionLinks = Array.from(document.querySelectorAll('ul.about-ul a'));
+
+    const sectionContent = Array.from(document.querySelectorAll('div.section-container'));
+
+    const linkBackground = '#7b7b7b';
+
+    sectionContent[0].style.display = 'block';
+
+    let currSection = sectionContent[0];
+
+    let currLink = sectionLinks[0];
+
+    for (let i = 0; i < sectionLinks.length; i++) {
+        sectionLinks[i].addEventListener('click', function () {
+
+            currSection.style.display = '';
+            currSection = sectionContent[i];
+            currSection.style.display = 'block';
+
+            currLink.style.backgroundColor = '';
+            currLink = sectionLinks[i];
+            currLink.style.background = linkBackground;
+
+        })
+
+    }
+})
